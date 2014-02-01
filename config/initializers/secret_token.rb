26776a26d189986734689d90f07eff33c1965ee3
brandenmittra <branden@univrs.co.uk>
2014-02-01 00:53:52 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Richterfan::Application.config.secret_key_base = 'd79052302227be4369d91a8d4fecbe264cfac5d90c454972818f9f695992ecc20aa19a591d0eb43ed8f25e60e385db1a14d13c3b764c7086dd0d9b7e11c81dda'
